33d69d7a0e305dce0ee7c41f47dbf76c3b29263e
seandbull <sean.bull@rbs.co.uk>
2022-12-01 15:25:18 +0000

    made a bit slicker and more re-usable

--- src/Day1.java
+++ src/Day1.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Day1 extends Day implements TwoPartProblem{
 
@@ -8,24 +9,11 @@ public class Day1 extends Day implements TwoPartProblem{
         super(filename);
     }
 
-    public  int part1() {
-        int biggestTotal = 0;
-        int currentTotal = 0;
-        for (String line : lines) {
-            if (line.isEmpty()) {
-                if (currentTotal > biggestTotal) {
-                    biggestTotal = currentTotal;
-                }
-                currentTotal = 0;
-            } else {
-                currentTotal += Integer.parseInt(line);
-            }
-        }
-        return biggestTotal;
-    }
-    public int part2(){
+    // Creation of a function that will take in the input as a List of Strings, accumulate the numbers between blank lines, and returns a sorted List.
+    public List<Integer> sumAndSort(){
         int currentTotal = 0;
         ArrayList<Integer> totals = new ArrayList<>();
+        // Loop through and sum the rows between the blank rows.
         for (String line : lines) {
             if (line.isEmpty()) {
                 totals.add(currentTotal);
@@ -36,7 +24,16 @@ public class Day1 extends Day implements TwoPartProblem{
             }
 
         }
+        // cheat sort :)
         totals.sort(Collections.reverseOrder());
+        return totals;
+    }
+
+    public  int part1() {
+        return sumAndSort().get(0);
+    }
+    public int part2(){
+        List<Integer> totals = sumAndSort();
         return totals.get(0) + totals.get(1) + totals.get(2);
 
     }
