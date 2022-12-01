52c2dd3dbc27d4a1bdf33c9294dd5c8ef38d0e56
seandbull <sean.bull@rbs.co.uk>
2022-12-01 15:16:13 +0000

    README added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Day1 day1 = new Day1("src/input.txt");
+        Day1 day1 = new Day1("resources/day1.txt");
         day1.sOutPart1();
         day1.sOutPart2();
     }
